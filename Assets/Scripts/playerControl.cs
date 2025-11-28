6c237bb24475efc81c87575bb55ce37b726f42ee
Ofer Afik <ofer.afik123@gmail.com>
2025-11-28 13:50:06 +0200

    Start working on animations

--- Assets/Scripts/playerControl.cs
+++ Assets/Scripts/playerControl.cs
@@ -5,16 +5,19 @@ public class playerControl : MonoBehaviour
     public float speed = 10f;
     private float x;
     private float y;
+    public Rigidbody2D rb;
+    public Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
     void FixedUpdate()
@@ -22,5 +25,6 @@ public class playerControl : MonoBehaviour
         x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(x, y, 0);
+        
     }
 }
