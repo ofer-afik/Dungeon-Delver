01029e4031bb8beb768fe948cb3b0549cd0c92fb
Ofer Afik <ofer.afik123@gmail.com>
2025-12-12 13:03:46 +0200

    Tweak a few things

--- Assets/Scripts/ReturnBut.cs
+++ Assets/Scripts/ReturnBut.cs
@@ -12,6 +12,9 @@ public class ReturnBut : MonoBehaviour
     public void OnPress()
     {
         SceneManager.LoadScene("StartScene");
+        GameManager.Instance.coins = 0;
+        GameManager.Instance.EnemiesKilled = 0;
+        GameManager.Instance.PlayerHP = 100;
         GameManager.Instance.dead = false;
     }
     // Update is called once per frame
